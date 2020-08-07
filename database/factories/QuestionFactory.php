df36e3f5134e4ff230c5fc333ee20aac807df74a
Jacey <jacey.j.bennett@gmail.com>
2020-08-07 12:14:54 -0400

    update question factory with comments

--- database/factories/QuestionFactory.php
+++ database/factories/QuestionFactory.php
@@ -7,6 +7,6 @@ use Faker\Generator as Faker;
 
 $factory->define(Question::class, function (Faker $faker) {
     return [
-        'question' => str_replace('.', "?", $faker->sentence($nbWords = 10, $variableNbWords = true)),
+        'question' => str_replace('.', "?", $faker->sentence($nbWords = 10, $variableNbWords = true)), // creates a random Lorem sentence and replaces the period with a question mark
     ];
 });
